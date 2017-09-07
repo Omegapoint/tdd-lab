b3a256440dbfa1d6a226fbeda9fdb8a239290742
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2017-09-07 10:05:28 +0200

    Begun Basket implementation

--- src/main/java/se/omegapoint/academy/basket/Basket.java
+++ src/main/java/se/omegapoint/academy/basket/Basket.java
@@ -1,8 +1,18 @@
 package se.omegapoint.academy.basket;
 
+import se.omegapoint.academy.basket.items.Candy;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Basket {
     private final List<Item> items = new ArrayList<>();
+    private int numberOfItems = 0;
+
+    public int getNumberOfItems() {
+        return numberOfItems;
+    }
+    public void addCandy(Candy candy) {
+        numberOfItems++;
+    }
 }
--- src/test/java/se/omegapoint/academy/basket/BasketTest.java
+++ src/test/java/se/omegapoint/academy/basket/BasketTest.java
@@ -1,5 +1,32 @@
 package se.omegapoint.academy.basket;
 
+import org.junit.Test;
+import se.omegapoint.academy.basket.items.Candy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BasketTest {
+
+    @Test
+    public void basket_is_empty_when_new() {
+        Basket basket = new Basket();
+
+        assertThat(basket.getNumberOfItems()).isEqualTo(0);
+    }
+
+
+    @Test
+    public void basket_has_candy() {
+        // Given
+        Basket basket = new Basket();
+        Candy candy = new Candy("Kexchoklad");
+
+        // When
+        basket.addCandy(candy);
+
+
+        // Then
+        assertThat(basket.getNumberOfItems()).isEqualTo(1);
+    }
 
 }
