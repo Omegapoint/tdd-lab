fab73224b5e859312b4b21515087d7330d2ecdc9
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2018-02-02 05:53:31 +0100

    Added Jolt

--- src/main/java/se/omegapoint/academy/tdd/webshop/Items.java
+++ src/main/java/se/omegapoint/academy/tdd/webshop/Items.java
@@ -9,4 +9,5 @@ public class Items {
     public static final Item VECKANS_NU = new Item(3, "Veckans nu!", BigDecimal.valueOf(29));
     public static final Item BRUN_BANAN = new Item(4, "Banan", BigDecimal.valueOf(7));
     public static final Item LORANGA = new Item(5, "Loranga", BigDecimal.valueOf(13));
+    public static final Item JOLT = new Item(5, "JOLT Cola", BigDecimal.valueOf(1450, 2));
 }
