9c82cd0eee67c810d2916bf6d40ae5182de118db
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-09-06 15:56:17 +0200

    mocked pricingservice

--- src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
+++ src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
@@ -4,17 +4,22 @@ import java.math.BigDecimal;
 
 public class ShoppingCart {
 
+    final PricingService pricingService;
     int counter;
     BigDecimal sum = new BigDecimal(0);
+
+    public ShoppingCart(PricingService pricingService) {
+        this.pricingService = pricingService;
+    }
 
     public int getAllItems() {
 
         return counter;
     }
 
-    public void addItem(Item brunBanan) {
+    public void addItem(Item item) {
         counter++;
-        sum = sum.add(brunBanan.getPrice());
+        sum = sum.add(pricingService.priceForItem(item.getItemId()));
 
     }
 
--- src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
+++ src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
@@ -1,6 +1,8 @@
 package se.omegapoint.academy.tdd.webshop;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.math.BigDecimal;
 
@@ -9,11 +11,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShoppingCartTest {
 
+
+    final PricingService pricingService = Mockito.mock(PricingService.class);
+
+    @Before
+    public void setUp(){
+        Mockito.when(pricingService.priceForItem(Mockito.anyInt())).thenReturn(new BigDecimal(0));
+    }
+
     @Test
     public void shopping_cart_is_empty_when_created(){
 
         //given
-        ShoppingCart shoppingCart = new ShoppingCart();
+        ShoppingCart shoppingCart = new ShoppingCart(pricingService);
 
         //when
         int numberOfItems = shoppingCart.getAllItems();
@@ -26,7 +36,7 @@ public class ShoppingCartTest {
     public void shopping_cart_has_one_item_when_one_item_is_added(){
 
         //given
-        ShoppingCart shoppingCart = new ShoppingCart();
+        ShoppingCart shoppingCart = new ShoppingCart(pricingService);
         shoppingCart.addItem(Items.BRUN_BANAN);
 
         //when
@@ -40,7 +50,7 @@ public class ShoppingCartTest {
     public void sum_of_empty_shopping_cart(){
 
         //given
-        ShoppingCart shoppingCart = new ShoppingCart();
+        ShoppingCart shoppingCart = new ShoppingCart(pricingService);
 
         //when
         BigDecimal sumOfItems = shoppingCart.getSum();
@@ -53,7 +63,9 @@ public class ShoppingCartTest {
     public void sum_of_shopping_cart(){
 
         //given
-        ShoppingCart shoppingCart = new ShoppingCart();
+        Mockito.when(pricingService.priceForItem(Mockito.anyInt())).thenReturn(new BigDecimal(7));
+
+        ShoppingCart shoppingCart = new ShoppingCart(pricingService);
         shoppingCart.addItem(Items.BRUN_BANAN);
         //when
         BigDecimal sumOfItems = shoppingCart.getSum();
