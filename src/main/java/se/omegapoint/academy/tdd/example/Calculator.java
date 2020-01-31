07dfb6513b5ec344c2ddeb633c44951aa125ee61
Andreas Forsberg <andreas.forsberg@tradera.com>
2020-01-31 10:59:04 +0100

    add slides to ppt and one simple test

---
+++ src/main/java/se/omegapoint/academy/tdd/example/Calculator.java
@@ -0,0 +1,8 @@
+package se.omegapoint.academy.tdd.example;
+
+public class Calculator {
+
+    public int Add(int x, int y) {
+        return x + y;
+    }
+}
---
+++ src/test/java/se/omegapoint/academy/tdd/example/CalculatorTest.java
@@ -0,0 +1,32 @@
+package se.omegapoint.academy.tdd.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setupCalculator() {
+        Calculator = new Calculator();
+    }
+
+    @Test
+    public void can_add() {
+        // given
+        final int x = 1;
+        final int y = 1;
+
+        // when
+        final sum = calculator.Add(x, y;)
+
+        // then
+        assertThat(sum).isEqualTo(2);
+    }
+}
