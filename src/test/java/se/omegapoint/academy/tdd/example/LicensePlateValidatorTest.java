ff7d6cc2d033286c9498acb10aa17948312d9763
Henrik Stensson <henrik.stensson@omegapoint.se>
2021-02-03 13:32:17 +0100

    Added empty test classes for LicencePlateValidator and ShoppingCart

---
+++ src/test/java/se/omegapoint/academy/tdd/example/LicensePlateValidatorTest.java
@@ -0,0 +1,26 @@
+package se.omegapoint.academy.tdd.example;
+
+import org.junit.Test;
+
+/**
+ * LicensePlateValidatorTest - tests for LicensePlateValidator
+ * Created: 2021-02-03
+ * Author: Henrik Stensson, henrik.stensson@omegapoint.se
+ */
+public class LicensePlateValidatorTest {
+
+    @Test
+    public void testCreate(){
+
+    }
+
+    @Test
+    public void correct_license_plate_should_validate_true() {
+
+    }
+
+    @Test
+    public void incorrect_license_plate_should_validate_false() {
+
+    }
+}
---
+++ src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
@@ -0,0 +1,9 @@
+package se.omegapoint.academy.tdd.webshop;
+
+/**
+ * ShoppingCartTest - tests for ShoppingCart
+ * Created: 2021-02-03
+ * Author: Henrik Stensson, henrik.stensson@omegapoint.se
+ */
+public class ShoppingCartTest {
+}
