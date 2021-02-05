39d99105e5445d02ce548899e49ff77af5b0b630
siumaii <andtan@omegapoint.se>
2021-02-05 10:14:40 +0100

    ShoppingCartTest2

--- src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
+++ src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
@@ -1,8 +1,25 @@
 package se.omegapoint.academy.tdd.webshop;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private List<Item> items = new ArrayList<>();
+
 
     public int returnNumberOfItems(){
-        return 0;
+        return items.size();
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+    public BigDecimal returnPrice(){
+        BigDecimal total = BigDecimal.ZERO;
+        for (Item i : items) {
+            total = total.add(i.getPrice());
+        }
+        return total;
     }
 }
--- src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
+++ src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
@@ -3,6 +3,8 @@ package se.omegapoint.academy.tdd.webshop;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 /**
  * ShoppingCartTest - tests for ShoppingCart
  * Created: 2021-02-03
@@ -24,6 +26,36 @@ public class ShoppingCartTest {
         //When
 
         //Then
-        Assert.assertEquals(shoppingCart.returnNumberOfItems(),0);
+        Assert.assertEquals(0, shoppingCart.returnNumberOfItems());
+    }
+
+    @Test
+    public void checkThatReturnNumberOfItemsIsOneAfterAddingOne(){
+        //Given
+        ShoppingCart shoppingCart = new ShoppingCart();
+        //When
+        shoppingCart.addItem(Items.KEXCHOKLAD);
+        //Then
+        Assert.assertEquals(1, shoppingCart.returnNumberOfItems());
+    }
+
+    @Test
+    public void checkShoppingCartPriceIsZeroWhenNoItems(){
+        //Given
+        ShoppingCart shoppingCart = new ShoppingCart();
+        //When
+
+        //Then
+        Assert.assertEquals(BigDecimal.ZERO, shoppingCart.returnPrice());
+    }
+
+    @Test
+    public void checkShoppingCartPriceIsCorrect(){
+        //Given
+        ShoppingCart shoppingCart = new ShoppingCart();
+        //When
+        shoppingCart.addItem(Items.KEXCHOKLAD);
+        //Then
+        Assert.assertEquals(BigDecimal.valueOf(14), shoppingCart.returnPrice());
     }
 }
