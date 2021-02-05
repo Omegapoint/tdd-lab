19ee35a3f990ec0b8eac70c0b94e9da7c89afb54
Amanda <amanda.utterback@hotmail.com>
2021-02-05 09:45:39 +0100

    ShoppingCartTest1

--- src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
+++ src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
@@ -1,4 +1,8 @@
 package se.omegapoint.academy.tdd.webshop;
 
 public class ShoppingCart {
+
+    public int returnNumberOfItems(){
+        return 0;
+    }
 }
--- src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
+++ src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
@@ -1,9 +1,29 @@
 package se.omegapoint.academy.tdd.webshop;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * ShoppingCartTest - tests for ShoppingCart
  * Created: 2021-02-03
  * Author: Henrik Stensson, henrik.stensson@omegapoint.se
  */
 public class ShoppingCartTest {
+    @Test
+    public void testCreate(){
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Assert.assertNotNull(shoppingCart);
+
+    }
+
+    @Test
+    public void giveBackZero(){
+        //Given
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        //When
+
+        //Then
+        Assert.assertEquals(shoppingCart.returnNumberOfItems(),0);
+    }
 }
