3e37c50bbf9f930dd871b7758d420cd147136505
Carl Rosengren <carl.rosengren@omegapoint.se>
2023-03-14 11:18:43 +0100

    Add license plate example

--- src/main/java/se/omegapoint/academy/tdd/example/LicensePlateValidator.java
+++ src/main/java/se/omegapoint/academy/tdd/example/LicensePlateValidator.java
@@ -1,7 +1,76 @@
 package se.omegapoint.academy.tdd.example;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class LicensePlateValidator {
-    public boolean isCorrect(String licensePlate) {
+    public boolean validate1(String licensePlate) {
         return true;
+    }
+
+    public boolean validate2(String licensePlate) {
+        for (int i = 0; i < 6; i++) {
+            char character = licensePlate.charAt(i);
+            if (i < 3 && !Character.isLetter(character)) {
+                return false;
+            } else if (i >= 3 && !Character.isDigit(character)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validate3(String licensePlate) {
+        for (int i = 0; i < 6; i++) {
+            char character = licensePlate.charAt(i);
+            if (i < 3 && !Character.isLetter(character)) {
+                return false;
+            } else if (i >= 3 && i <=4 && !Character.isDigit(character)) {
+                return false;
+            } else if (i == 5 && !Character.isLetterOrDigit(character)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validate4(String licensePlate) {
+        if (licensePlate.length() != 6) return false;
+        for (int i = 0; i < 6; i++) {
+            char character = licensePlate.charAt(i);
+            if (i < 3 && !Character.isLetter(character)) {
+                return false;
+            } else if (i >= 3 && i <=4 && !Character.isDigit(character)) {
+                return false;
+            } else if (i == 5 && !Character.isLetterOrDigit(character)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validate5(String licensePlate) {
+        // ABCDEFGH _I_ JKLMNOP _Q_ RSTU _V_ WXYZ
+        // A  -   H     J  -  P     R- U     W- Z
+        // A-H          J-P         R-U      W-Z
+        // A-HJ-PR-UW-Z
+        // Notera skillnaden i sista reguljära uttrycket (med O)
+        return licensePlate.matches("[A-HJ-PR-UW-Z]{3}[0-9]{2}[A-H-J-NPR-UW-Z1-9]");
+    }
+
+    public boolean validate(String licensePlate) {
+        if (licensePlate.length() != 6) return false;
+
+        try {
+            List<String> illegalWords = Files.readAllLines(Path.of("src/main/resources/illegalWords.txt"));
+            if (illegalWords.contains(licensePlate.substring(0, 3))) {
+                return false;
+            }
+        } catch (Exception e) {
+            // handle
+        }
+
+        return licensePlate.matches("[A-HJ-PR-UW-Z]{3}[0-9]{2}[A-H-J-NPR-UW-Z1-9]");
     }
 }
--- src/test/java/se/omegapoint/academy/tdd/example/LicensePlateValidatorTest.java
+++ src/test/java/se/omegapoint/academy/tdd/example/LicensePlateValidatorTest.java
@@ -1,15 +1,10 @@
 package se.omegapoint.academy.tdd.example;
 
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 /**
  * LicensePlateValidatorTest - tests for LicensePlateValidator
@@ -20,40 +15,63 @@ import static org.mockito.Mockito.mock;
  *  Carl Rosengren
  */
 public class LicensePlateValidatorTest {
-    HttpClient mockHttpClient = mock(HttpClient.class);
-
-    @AfterEach
-    public void noMoreInteractions(){
-        Mockito.verifyNoMoreInteractions(
-                mockHttpClient
-        );
-    }
-
+    LicensePlateValidator licensePlateValidator = new LicensePlateValidator();
 
     @Test
-    public void testCreate(){
-
-    }
-
-    @Test
-    public void correct_license_plate_should_validate_true() {
+    public void three_letters_three_digits_should_be_valid() {
         // given
+        licensePlateValidator = new LicensePlateValidator();
+        String validLicensePlate = "ABC123";
+
         // when
+        boolean result = licensePlateValidator.validate(validLicensePlate);
+
         // then
-
-        // new Mock
-        assertTrue(new LicensePlateValidator().isCorrect("AAA000"));
-        // assertTrue(new LicensePlateValidator(mockHttpClient).isCorrect("AAA000"));
-
-        Mockito.any();
-        Matchers.any();
-        Mockito.when(new LicensePlateValidator().isCorrect("Test")).thenReturn(true);
-        BDDMockito.given(new LicensePlateValidator().isCorrect("Test")).willReturn(true);
+        assertTrue(result);
     }
 
     @Test
-    public void incorrect_license_plate_should_validate_false() {
-        assertFalse(new LicensePlateValidator().isCorrect("AAAAAA"));
+    public void six_letters_should_be_invalid() {
+        // given
+        licensePlateValidator = new LicensePlateValidator();
+        String invalidLicensePlate = "ABCDEF";
 
+        // when
+        boolean result = licensePlateValidator.validate(invalidLicensePlate);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    public void three_letters_two_digits_one_letter_should_be_valid() {
+        assertTrue(licensePlateValidator.validate("ABC12A"));
+    }
+
+    @Test
+    public void too_short_or_long_plate_should_be_invalid() {
+        assertFalse(licensePlateValidator.validate("A"));
+        assertFalse(licensePlateValidator.validate("AAAAAAAAAAAAA"));
+    }
+
+    @Test
+    public void illegal_characters_should_be_invalid() {
+        /*
+        I - används ej, förväxlingsbar med J (handstil/uttal) och L (gement: l)
+        O - Används inte som sista bokstav om det är 2 siffror och en fjärde bokstav, förväxlingarbar med 0
+        Q - Förväxlingsbar med O
+        V - Förväxlingsbar med U (handstil) och W (talspråk)
+        (Tekniskt sett kan dessa användas på personliga skyltar)
+         */
+
+        assertFalse(licensePlateValidator.validate("ABI00A"));
+        assertFalse(licensePlateValidator.validate("ABQ001"));
+        assertFalse(licensePlateValidator.validate("ABV002"));
+        assertFalse(licensePlateValidator.validate("ABC00O"));
+    }
+
+    @Test
+    public void illegal_words_should_be_invalid() {
+        assertFalse(licensePlateValidator.validate("OND123"));
     }
 }
