e998d2e41e411e5f96001049198c82ea1cc3f579
Joel Gerdin <joel.gerdin@omegapoint.se>
2023-12-13 14:12:44 +0100

    Done with part 2

--- src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
+++ src/main/java/se/omegapoint/academy/tdd/webshop/ShoppingCart.java
@@ -1,4 +1,27 @@
 package se.omegapoint.academy.tdd.webshop;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private List<Item> itemList;
+
+    public ShoppingCart() {
+        itemList = new ArrayList<>();
+    }
+
+    public int getNumberOfItems() {
+        return itemList.size();
+    }
+
+    public void add(Item item) {
+        itemList.add(item);
+    }
+
+    public BigDecimal getCombinedPrice() {
+        return itemList.stream()
+                .map(Item::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
--- src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
+++ src/test/java/se/omegapoint/academy/tdd/webshop/ShoppingCartTest.java
@@ -1,9 +1,47 @@
 package se.omegapoint.academy.tdd.webshop;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 /**
  * ShoppingCartTest - tests for ShoppingCart
  * Created: 2021-02-03
  * Author: Henrik Stensson, henrik.stensson@omegapoint.se
  */
 public class ShoppingCartTest {
+
+    @Test
+    void givenNoAddedItemsBasketShouldReturnZero() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Assertions.assertEquals(0, shoppingCart.getNumberOfItems());
+    }
+
+    @Test
+    void givenAddedItemsBasketShouldReturnNumberOfItems() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        // added
+        shoppingCart.add(Items.KEXCHOKLAD);
+        Assertions.assertEquals(1, shoppingCart.getNumberOfItems());
+    }
+
+    @Test
+    void givenAddedItemsBasketShouldReturnCombinedPrice() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        // added
+        shoppingCart.add(Items.KEXCHOKLAD);
+        shoppingCart.add(Items.BILAR);
+        BigDecimal expectedPrice = Items.KEXCHOKLAD.getPrice().add(Items.BILAR.getPrice());
+        Assertions.assertEquals(expectedPrice, shoppingCart.getCombinedPrice());
+    }
+
+    @Test
+    void givenNoAddedItemsBasketShouldReturnZeroCombinedPrice() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BigDecimal expectedPrice = BigDecimal.ZERO;
+        Assertions.assertEquals(expectedPrice, shoppingCart.getCombinedPrice());
+    }
+
+
 }
